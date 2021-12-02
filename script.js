6e3bc944df0c92597c295cbbf72a60d8324ab9ea
GustavoPercastre <angelpercastre293@gmail.com>
2021-12-01 20:30:57 -0800

    Created the function reliableMultiply which will take to variables (a,b).

--- script.js
+++ script.js
@@ -10,4 +10,17 @@
 
 class MultiplicatorUnitFailure extends Error {}
 
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) 
+{
+  
+}
+
 
